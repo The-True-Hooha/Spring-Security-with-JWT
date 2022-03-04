b22bed54070fc40fb1c8705c278333746a474381
David Ogar <owogogahhero@outlook.com>
2022-03-04 13:28:10 +0100

    added user roles

---
+++ src/main/java/com/TheTrueHooha/SpringSecurityJWT/Models/AppRole.java
@@ -0,0 +1,4 @@
+package com.TheTrueHooha.SpringSecurityJWT.Models;
+
+public class AppRole {
+}
---
+++ src/main/java/com/TheTrueHooha/SpringSecurityJWT/Models/AppUser.java
@@ -0,0 +1,29 @@
+package com.TheTrueHooha.SpringSecurityJWT.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //user parameters
+    private long id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER) //loads all the roles when a user is fetched
+    private Collection<AppRole> roles = new ArrayList<>();
+}
